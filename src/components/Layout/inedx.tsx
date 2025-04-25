1b00c3691bba74df70c715f69df3922d78383388
linyangxin-cn <1227143051@qq.com>
2025-04-25 17:38:02 +0800

    feat: 处理全局央视

--- src/components/Layout/inedx.tsx
+++ src/components/Layout/inedx.tsx
@@ -2,6 +2,15 @@ import { routes } from "@/routes";
 import React, { useMemo } from "react";
 import { useLocation } from "react-router-dom";
 import styles from "./index.module.less";
+import { Header } from "antd/es/layout/layout";
+import headerIcon from "@/assets/header-icon.svg";
+import { Input } from "antd";
+import {
+  QuestionCircleOutlined,
+  SearchOutlined,
+  SettingOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
 
 interface LayoutProps {
   children?: React.ReactNode;
@@ -18,7 +27,26 @@ const Layout: React.FC<LayoutProps> = (props) => {
 
   return (
     <div className={styles.layout}>
-      {showMenu && <div>layout</div>}
+      {showMenu && (
+        <Header className={styles.header}>
+          <img src={headerIcon} alt="" />
+          <div className={styles.searchBar}>
+            <Input
+              placeholder="Search keywords"
+              prefix={<SearchOutlined style={{ color: "rgb(101,104,113)" }} />}
+              style={{
+                backgroundColor: "transparent",
+                border: "1px solid rgb(101,104,113)",
+              }}
+            />
+          </div>
+          <div className={styles.rightIcons}>
+            <QuestionCircleOutlined style={{ color: "#fff" }} />
+            <SettingOutlined style={{ color: "#fff" }} />
+            <UserOutlined style={{ color: "#fff" }} />
+          </div>
+        </Header>
+      )}
       {children}
     </div>
   );
